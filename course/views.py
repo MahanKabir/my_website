f6953ea857e2d1811b702a3036e8e4ff8239cdc8
poulstar <info@poulstar.com>
2020-12-09 16:15:36 +0330

    complete course create

--- course/views.py
+++ course/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
+from .forms import CourseForm
 # Create your views here.
 
 
 def create(request):
-    pass
+    if request.method == "POST":
+        form = CourseForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+
+    return render(request, 'course/create.html')
 
 def read(request):
     pass
